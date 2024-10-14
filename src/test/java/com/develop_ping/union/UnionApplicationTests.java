8c1fb52e13059c2b16ee478c1810904e2b3061a9
eeezi <alloy0035@hanmail.net>
2024-10-14 19:08:12 +0900

    project init

---
+++ src/test/java/com/develop_ping/union/UnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop_ping.union;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
