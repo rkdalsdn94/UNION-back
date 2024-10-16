9053157066904a600a24e2ea27295e4da8b05e66
minwooKang <rkdalsdn94@gmail.com>
2024-10-16 16:55:49 +0900

    chore: 개발 환경 셋팅(#1)

--- src/test/java/com/develop_ping/union/UnionApplicationTests.java
+++ src/test/java/com/develop_ping/union/UnionApplicationTests.java
@@ -1,13 +1,20 @@
 package com.develop_ping.union;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UnionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @DisplayName("Union 컨텍스트가 로드되어야 한다.")
+    @Test
+    void contextLoads() {
+        UnionApplication application = new UnionApplication();
+        assertNotNull(application);
+    }
 }
