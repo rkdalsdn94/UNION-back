4809aeb0ea33ede282774aefb24c10796880b7d7
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-10-27 08:58:53 +0900

    Revert "bug: post user null 확인을 위한 로그 추가(#59)"
    This reverts commit 347dac8abf76c1832aa57a46eddbb085b5e9e2c8.

--- src/main/java/com/develop_ping/union/post/domain/service/PostServiceImpl.java
+++ src/main/java/com/develop_ping/union/post/domain/service/PostServiceImpl.java
@@ -23,7 +23,6 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public PostInfo createPost(PostCreationCommand command) {
         log.info("[ CALL: PostService.createPost() ] title: {}",command.getTitle());
-        log.info("[ USER ID: {} ]", command.getUser().getId());
 
         User user = command.getUser();
 
@@ -41,7 +40,6 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public PostInfo updatePost(PostUpdateCommand command) {
         log.info("[ CALL: PostService.updatePost() ] post id: {}", command.getId());
-        log.info("[ USER ID: {} ]", command.getUser().getId());
 
         Post post = postManager.findById(command.getId());
         User user = command.getUser();
@@ -59,7 +57,6 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public void deletePost(PostDeleteCommand command) {
         log.info("[ CALL: PostService.deletePost() ] post id: {}", command.getPostId());
-        log.info("[ USER ID: {} ]", command.getUser().getId());
 
         Post post = postManager.findById(command.getPostId());
         User user = command.getUser();
@@ -83,7 +80,7 @@ public class PostServiceImpl implements PostService {
     }
 
     private void validatePostOwner(User user, Post post) {
-        if (!user.getId().equals(post.getUser().getId())) {
+        if (!user.equals(post.getUser())) {
             throw new PostPermissionDeniedException(user.getId(), post.getId());
         }
     }
