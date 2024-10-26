a1a7863758584224f3f4fbc6ec71a803099532db
minwooKang <rkdalsdn94@gmail.com>
2024-10-26 15:12:53 +0900

    fix: 모임 생성 시간 형식 프론트에 맞춰 변경 (#56)

--- src/main/java/com/develop_ping/union/gathering/presentation/dto/request/GatheringRequest.java
+++ src/main/java/com/develop_ping/union/gathering/presentation/dto/request/GatheringRequest.java
@@ -27,7 +27,7 @@ public class GatheringRequest {
     private Integer currentMember;
 
     @NotNull
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private ZonedDateTime gatheringDateTime;
     private String address;
     private Double latitude;
