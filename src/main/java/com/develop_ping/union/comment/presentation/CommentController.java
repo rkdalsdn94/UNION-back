d09d9b92921274711748622b65cda6513a98c468
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-10-27 09:34:55 +0900

    feat: cbuild ommentControllerr

---
+++ src/main/java/com/develop_ping/union/comment/presentation/CommentController.java
@@ -0,0 +1,9 @@
+package com.develop_ping.union.comment.presentation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class CommentController {
+}
